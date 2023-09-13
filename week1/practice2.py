fd5dfe388c45f2789048057eaedbeda887138b41
ds-page <82150280+ds-midterm-page@users.noreply.github.com>
2023-09-13 13:08:25 +0600

    Add files via upload

---
+++ week1/practice2.py
@@ -0,0 +1,53 @@
+import math
+
+#1.a
+#a = float(input())
+#print(a * 2)
+#2.a
+#b = input()
+#print(b + "\n" + b)
+#3.a
+#c = float(input())
+#print(c**3)
+
+#1.b
+#d = int(input())
+#print(chr(d + 64) + " " + chr(d + 96))
+#2.b
+#e = float(input())
+#print(int(e))
+#3.c
+#a = float(input())
+#b = float(input())
+#
+#c = math.sqrt(a * a + b * b)
+#print(c)
+
+#1.c
+#d = float(input())
+
+#if int(d) < d:
+# print(int(d))
+#else:
+# print(math.ceil(d))
+
+#2.c
+
+#a = int(input())
+
+#hours = a / 3600
+#minutes = ((a / 60) % 60)
+#seconds = a % 60
+
+#print(int(hours), ":", int(minutes), ":", int(seconds))
+
+#3.c
+
+#xa = int(input())
+#ya = int(input())
+#xb = int(input())
+#yb = int(input())
+
+#AB = math.sqrt((xa - xb)**2 + (ya - yb)**2)
+
+#print(AB)
